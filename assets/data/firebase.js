aa499df8d57f8563bb955cc445afe4109e805219
Rebertt Rodrigues <joaorebertt0219@gmail.com>
2021-10-05 17:02:17 -0300

    create function delete content

--- assets/data/firebase.js
+++ assets/data/firebase.js
@@ -9,20 +9,31 @@
         messagingSenderId: "347902797715",
         appId: "1:347902797715:web:6d31c0c8cae973f9582a66"
     };
+
+
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
+
+
+
+
 
     // Declarando Variaveis 
     const db = firebase.firestore();
     const form = document.getElementById("forms");
     const btnForm = document.getElementById("btn-form");
-    const deletar = document.querySelectorAll(".btn-del")
+    const deletar = document.querySelectorAll("btn-del");
   
+
+
     // Função para criar enviar os dados ao banco
-  btnForm.addEventListener('click',(e) => {
+    btnForm.addEventListener('click',(e) => {
       e.preventDefault();
       
+
+
     //   Variaveis com o valor do inpunt 
+
       let paciente = "pacientes";
 
       let nome = document.querySelector('#inputName').value;
@@ -43,8 +54,9 @@
       let end = document.querySelector('#inputAddress').value;
       let bairro = document.querySelector('#inputBairro').value;
 
-    //   Enviando dados para o banco
 
+
+    //   Enviando dados para o banco
       db.collection(paciente).add({
           Nome:nome,
           CPF:cpf,
@@ -71,44 +83,53 @@
       .catch((err) => {
               console.log("Ops,Não foi possível finalizar o cadastro,Chama o Rebertt",err);
       });
+
       form.reset();
   });
 
 
-// ler todos os dados de uma coleção!!
+
+
+// Lendo todos os dados de uma coleção em tempo real!!
 db.collection("pacientes").onSnapshot((data)=>{
   const table = document.getElementById("lisTable");  
   data.docs.map((val)=>{
 
-    table.innerHTML += 
-    `<tr>
-    <td>4</td>
-    <td>${val.data().Nome   }</td>
-    <td>${val.data().CPF}</td>
-    <td class="align-middle">
-      <div class="progress" data-height="4" data-toggle="tooltip" title="100%">
-        <div class="progress-bar bg-success" data-width="100"></div>
-      </div>
-    </td>
-    <td>2021-09-16</td>
-    <td><div class="badge badge-success">Ativo</div></td>
-    <td><a href="#" class="btn btn-secondary">Detalhes</a></td>
-    <td><a href="#" class="btn btn-success">Editar</a></td>
-    <td><button class="btn btn-danger del">Apagar</button></td>
-    </tr>
-  `
+
+    // O codigo apenas funciona após remover o esses inner
+
+  //   table.innerHTML += 
+
+  //   `<tr>
+  //   <td>4</td>
+  //   <td>${val.data().Nome   }</td>
+  //   <td>${val.data().CPF}</td>
+  //   <td class="align-middle">
+  //     <div class="progress" data-height="4" data-toggle="tooltip" title="100%">
+  //       <div class="progress-bar bg-success" data-width="100"></div>
+  //     </div>
+  //   </td>
+  //   <td>${val.data().Nascimento}</td>
+  //   <td><div class="badge badge-success">Ativo</div></td>
+  //   <td><a href="#" class="btn btn-secondary">Detalhes</a></td>
+  //   <td><a href="#" class="btn btn-success">Editar</a></td>
+  //   <td><button class="btn btn-danger" id="btn-del">Apagar</button></td>
+  //   </tr>
+  // `
+
+  
 
     });
-    
-});
-
-// Deletando conteudo
-
-deletar.forEach(element => {
 
 });
 
-element.addEventListener('click',(e)=>{
-  e.preventDefault();
-  alert("deu certoooo");
-});
+
+
+
+// Deletando conteudos
+deletar.addEventListener("click",deletando);
+function deletando(){
+  alert("deu certo"); 
+}
+
+ 
