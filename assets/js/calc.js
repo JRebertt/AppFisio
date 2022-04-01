593258c4654e5650db6d9f87bd66a4a39ecc6dd1
Rebertt Rodrigues <joaorebertt0219@gmail.com>
2022-04-01 10:45:24 -0300

    Update

---
+++ assets/js/calc.js
@@ -0,0 +1,24 @@
+
+
+
+// function calcIMC () {
+
+ 
+
+
+// }
+
+
+// let peso = document.getElementById('inputPeso').value;
+// let alt = document.getElementById('inputAlt'.value);
+// let MC = document.getElementById('inputMC').value;
+// let MQ = document.getElementById('inputMQ').value;
+
+// peso = 120;
+// alt = 1 80;
+
+// console.log(peso, alt, MC, MQ);
+
+// resultIMC = peso / (alt * alt);
+
+// console.log(resultIMC)
